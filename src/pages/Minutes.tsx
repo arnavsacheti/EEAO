1f8df284284c37da39f1a62044220d63ee33e8dc
nobody172 <shawngreenwood172@gmail.com>
2024-04-10 18:30:52 +0000

    Added my name to minutes

--- src/pages/Minutes.tsx
+++ src/pages/Minutes.tsx
@@ -1,6 +1,14 @@
 const MinutesPage = () => {
     return (
-        <div>Minutes</div>
+        <>
+            <div>Minutes (Woah that's a lot of minutes!)</div>
+            <div> Group members:
+                <p>
+                    Shawn Greenwood - Information Privacy, Work and Wealth 
+                    Insert more here
+                </p>
+            </div>
+        </>
     )
 };
 
