b411dca6876484decbce87a795768b82052b5ef1
arnavsacheti <arnavsacheti@outlook.com>
2024-04-22 13:28:06 -0400

    lint + format + build

--- src/pages/Sections/index.tsx
+++ src/pages/Sections/index.tsx
@@ -1,2 +1,2 @@
-export { default as PATGPage } from "./PATG"
-export { default as NCPage } from "./NetworkCommunications"
+export { default as PATGPage } from "./PATG";
+export { default as NCPage } from "./NetworkCommunications";
