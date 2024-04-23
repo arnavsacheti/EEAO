faf382090144495614ea992afc04a263b8618efc
arnavsacheti <arnavsacheti@outlook.com>
2024-04-23 12:04:14 -0400

    added some details

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,3 +1,4 @@
+import { Grid, Stack, Typography } from "@mui/material";
 import TechnologyCard from "../components/TechnologyCard";
 
 const technologyItems = [
@@ -8,11 +9,41 @@ const technologyItems = [
     description:
       "Technology that acts as connection from main verse to another allowing a user to gain skills from alternate versions of themselves",
   },
+  {
+    name: "Lifepath Map",
+    image:
+      "https://static.wikia.nocookie.net/everythingeverywhere/images/7/73/Multiverse.jpg",
+    description:
+      "Map that shows all the decision that a persons has made in their life.",
+  },
 ];
 
 const TechnologyPage = () => {
   return (
     <>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Typography variant="h2" component="h1">
+            Technology
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack
+            direction="row"
+            alignItems="stretch"
+            justifyContent="space-between"
+            sx={{ width: "100%" }}
+          >
+            <Typography variant="subtitle1" color="text.secondary">
+              Social Implications of Computing Movie Analysis Project
+            </Typography>
+            {/* <Typography variant="subtitle1" color="text.secondary">
+            Everything Everywhere All at Once
+          </Typography> */}
+          </Stack>
+        </Grid>
+        <Grid item xs={12} />
+      </Grid>
       {technologyItems.map((item) => (
         <TechnologyCard
           name={item.name}
