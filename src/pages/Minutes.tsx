b6bab0481b8d755fc9a3245ee2fc19131be1425c
arnavsacheti <arnavsacheti@outlook.com>
2024-04-26 13:50:13 -0400

    lint + format + build

--- src/pages/Minutes.tsx
+++ src/pages/Minutes.tsx
@@ -2,25 +2,25 @@ import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
 
 const groupMembers = [
   {
-    name: 'Shawn Greenwood',
-    role: 'Information Privacy, Work and Wealth',
-    image: '/path/to/image-shawn.jpg' // Path to Shawn's image
+    name: "Shawn Greenwood",
+    role: "Information Privacy, Work and Wealth",
+    image: "/path/to/image-shawn.jpg", // Path to Shawn's image
   },
   {
-    name: 'Arnav Sacheti',
-    role: 'Privacy and The Government, Website',
-    image: '/path/to/image-arnav.jpg' // Path to Arnav's image
+    name: "Arnav Sacheti",
+    role: "Privacy and The Government, Website",
+    image: "/path/to/image-arnav.jpg", // Path to Arnav's image
   },
   {
-    name: 'Alexander Kraemling',
-    role: 'Intellectual Property, Computer Reliability',
-    image: '/path/to/image-alexander.jpg' // Path to Alexander's image
+    name: "Alexander Kraemling",
+    role: "Intellectual Property, Computer Reliability",
+    image: "/path/to/image-alexander.jpg", // Path to Alexander's image
   },
   {
-    name: 'Jan Pilja',
-    role: 'Network Communications, Computer & Network Security',
-    image: '/path/to/image-jan.jpg' // Path to Jan's image
-  }
+    name: "Jan Pilja",
+    role: "Network Communications, Computer & Network Security",
+    image: "/path/to/image-jan.jpg", // Path to Jan's image
+  },
 ];
 
 const MinutesPage = () => {
@@ -49,19 +49,19 @@ const MinutesPage = () => {
         </Grid>
         <Grid item xs={12} />
       </Grid>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
         <Typography variant="h5" gutterBottom>
           Group Members:
         </Typography>
-        {groupMembers.map(member => (
-          <Card key={member.name} sx={{ display: 'flex' }}>
+        {groupMembers.map((member) => (
+          <Card key={member.name} sx={{ display: "flex" }}>
             {/* <CardMedia
               component="img"
               sx={{ width: 151 }}
               image={member.image}
               alt={member.name}
             /> */}
-            <CardContent sx={{ flex: '1 0 auto' }}>
+            <CardContent sx={{ flex: "1 0 auto" }}>
               <Typography variant="h6">{member.name}</Typography>
               <Typography variant="body2" color="text.secondary">
                 {member.role}
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,11 +1,11 @@
 import { Grid, Stack, Typography } from "@mui/material";
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
 
 const TimelinePage = () => {
   return (
@@ -34,48 +34,49 @@ const TimelinePage = () => {
         <Grid item xs={12} />
       </Grid>
       <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          09:30 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          10:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          12:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Sleep</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary">
-          9:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Repeat</TimelineContent>
-      </TimelineItem>
-    </Timeline >
-    </>);
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            09:30 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>Eat</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            10:00 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>Code</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            12:00 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>Sleep</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent color="text.secondary">
+            9:00 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>Repeat</TimelineContent>
+        </TimelineItem>
+      </Timeline>
+    </>
+  );
 };
 
 export default TimelinePage;
